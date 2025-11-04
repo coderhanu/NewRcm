89a5f48738f2ecce63e8907ef4f8b3cfe5aee376
Gagan Tayal <gagantayal05@gmail.com>
2025-11-04 14:10:07 +0530

    Updated project code or design again

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css'
 import Navbar from './Component/Navbar/Navbar'
 import Hero from './Component/Hero/Hero'
 import Programs from './Component/Programs/Programs'
-import Title from './Component/Title/Title'
+// import Title from './Component/Title/Title'
 import Programm from './Component/Programm/Programm'
 import Footer from './Component/Footer/Footer'
-import Copyright from './Component/copyright/Copyright'
+import Copyright from './Component/Copyright/Copyright'
 
 function App() {
 
